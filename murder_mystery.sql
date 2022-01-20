e704895cf5cb916361a6a53d0efec3c1e4e4065e
Cornflake3000 <85275304+Cornflake3000@users.noreply.github.com>
2022-01-20 23:05:55 +0100

    finding out about two witnesses

--- murder_mystery.sql
+++ murder_mystery.sql
@@ -2,3 +2,9 @@
 
 -- Asking for crime scene report (date, type of crime and city were given: Jan. 15th 2018, murder in SQL City)
 SELECT * FROM crime_scene_report WHERE date = 20180115 AND type = 'murder' AND city = 'SQL City';
+
+/* Security footage shows that there were 2 witnesses. 
+The first witness lives at the last house on "Northwestern Dr". 
+The second witness, named Annabel, lives somewhere on "Franklin Ave". */
+
+-- Visiting the first witness
