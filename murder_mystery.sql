901b30523be48adb55359eb94afef6d4215f0856
Cornflake3000 <85275304+Cornflake3000@users.noreply.github.com>
2022-01-21 23:53:27 +0100

    identifying the witnesses

--- murder_mystery.sql
+++ murder_mystery.sql
@@ -7,4 +7,20 @@ SELECT * FROM crime_scene_report WHERE date = 20180115 AND type = 'murder' AND c
 The first witness lives at the last house on "Northwestern Dr". 
 The second witness, named Annabel, lives somewhere on "Franklin Ave". */
 
--- Visiting the first witness
+-- Identifying the first witness
+SELECT *, MAX(address_number) FROM person WHERE address_street_name = 'Northwestern Dr' 
+/* 
+Morty Schapiro
+Northwestern Dr 4919
+ID: 14887
+licence ID: 118009 
+ssn: 111564949 */
+
+-- Identifying the second witness
+SELECT * FROM person WHERE address_street_name = 'Franklin Ave' AND name LIKE 'Annabel %'
+/* 
+Annabel Miller
+Franklin Ave 103
+ID: 16371
+licence ID: 490173 
+ssn: 318771143 */
